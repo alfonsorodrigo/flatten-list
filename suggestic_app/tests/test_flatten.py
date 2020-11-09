fd28603712ba72fa48fe28b4ab6b8acbc66de75c
Alfonso Feria <alfonsorodrigo12@gmail.com>
2020-11-09 01:21:35 -0500

    Added Testing API Flatten

--- suggestic_app/tests/test_flatten.py
+++ suggestic_app/tests/test_flatten.py
@@ -9,15 +9,16 @@ sys.path.append(os.getcwd())
 
 from utils.flatten import flatten_list
 from models.flatten import Flatten, Base
+from app import app as main
 
 
 class FlattenListTestCase(unittest.TestCase):
     def setUp(self):
-        self.input_list_filled = [1, 2, [3, 4, [5, 6], 7], 8]
+        self.input_list = [1, 2, [3, 4, [5, 6], 7], 8]
         self.input_list_empty = []
 
     def test_flatten_list_filled(self):
-        response_flatten_list = flatten_list(self.input_list_filled, [])
+        response_flatten_list = flatten_list(self.input_list, [])
         self.assertListEqual(response_flatten_list, [1, 2, 3, 4, 5, 6, 7, 8])
         self.assertEqual(type(response_flatten_list), list)
         self.assertEqual(sum(response_flatten_list), 36)
@@ -57,6 +58,20 @@ class ORMCreateExampleDataTestCase(TestCase):
         self.session.commit()
         self.assertEqual(flatten_saved.result, "[1, 2, 3, 4, 5, 6, 7, 8]")
         self.assertNotEqual(flatten_saved.items, flatten_saved.result)
+
+
+class ApiFlattenTestCase(unittest.TestCase):
+    def setUp(self):
+        self.input_list = [1, 2, [3, 4, [5, 6], 7], 8]
+
+    def test_api_saved_data(self):
+        tester = main.test_client(self)
+        response = tester.post("flatten-list", json={"items": self.input_list})
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, dict)
+        self.assertIsInstance(response.json.get("result", []), list)
+        self.assertEqual(response.json.get("result", []), [1, 2, 3, 4, 5, 6, 7, 8])
+        self.assertEqual(sum(response.json.get("result", [])), 36)
 
 
 if __name__ == "__main__":
